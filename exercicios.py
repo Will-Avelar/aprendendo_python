d3001fe2f81a21ee52b20e6829ae4a3bc513e9e1
will avelar <willavelar@willavelar.onmicrosoft.com>
2023-01-26 23:02:17 -0300

    a

---
+++ exercicios.py
@@ -0,0 +1,49 @@
+'''33 palavras reservadas em payton == and, as, assert, break, class, continue, for, del, elif, else, except, false, finaly
+lambda, form, global, if, import, in, is, return, none, nonlocal, not, or, pass, raise, true, try, while, with, yield, def''' 
+
+"""instalado bibliotecas - no teminal devera ser usado o seguinte exemplo: pip install pysimplegui"""
+
+#imprimir na tela 
+print("Hello World") 
+
+#imprimir na tela com quebra de linha \n
+print("Hello\nWorld")
+
+
+a = 5
+b = 3
+print(a + b) #somou a + B 
+
+
+a = 5
+b = 3
+print(a, b)  #concatenou a, b
+
+a = 1 # esse argumento esta servindo como estrutura para os comando abaixo
+b = 2 # esse argumento esta servindo como estrutura para os comando abaixo
+
+
+print(a < b) # se um é menor que o outro 
+print(a > b) #se um é maior que o outro 
+print(a == b) #se um é igual ao outro 
+print(a != b) #se um é diferente do outro
+print(a <= b) #se um é menor ou igual ao outro
+print(a >= b) #se um é maior ou igual ao outro
+
+if b > a:
+    print("B é maior que A") #se B for maior que A vai imprimir B é maior que A
+else:
+    print("Bug") #se B for menor que A vai imprimir  bug
+
+print
+
+if 1 > 2:
+    print("1 é maior que 2") #se 1 for maior que 2 vai imprimir a palavra errado, pq a primeira checagem viu que é false então pulou para segunda checagem
+
+else:
+    print("errado")
+
+
+
+
+
